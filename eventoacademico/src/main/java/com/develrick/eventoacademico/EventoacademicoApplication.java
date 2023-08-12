a74f619713d1e5f3a17ae83609905558c9137281
Elrick Fernando <elrickfernando60@gmail.com>
2023-08-12 00:09:25 -0300

    Implementação das entidades Atividade e Categoria ORM

---
+++ eventoacademico/src/main/java/com/develrick/eventoacademico/EventoacademicoApplication.java
@@ -0,0 +1,13 @@
+package com.develrick.eventoacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoacademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoacademicoApplication.class, args);
+	}
+
+}
